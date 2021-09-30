f2ebcab335bc6eb57fae2edd24828859cb2ea670
UaynaNimtz <48319530+UaynaNimtz@users.noreply.github.com>
2021-09-29 21:36:21 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+console.log("Segunda Aula")
+console.log("Conversor");
+
+function Converter(){
+	//DOM - Document Object Model
+
+	/** PEGAR VALOR DO CAMPO */
+	// let valorEmDolar = document.getElementById("valor").value=100
+	
+	let valorEmDolar= document.querySelector("#valor").value
+
+	/** CONVERTER O VALOR DO CAMPO EM NÚMERO */
+	let valorEmDolarNumerico = parseFloat(valorEmDolar)
+
+	let cotacaoDolar = 5.42
+	let valorEmReal = valorEmDolarNumerico*cotacaoDolar
+
+	/** INSERIR O RESULTADO NA TELA */
+	// console.log(valorEmReal)
+
+	document.querySelector("#valorConvertido").innerHTML = "O resultado em real é R$" + valorEmReal.toFixed(2)
+}
